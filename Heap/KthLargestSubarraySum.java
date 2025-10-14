414b9b30f646acd5f680b0d2de486e305374716f
Nishant Bhalla <nishantbhalla32@gmail.com>
2025-10-14 22:49:39 +0530

    Add implementation for KthLargestSubarraySum to find the kth largest subarray sum in an array

---
+++ Heap/KthLargestSubarraySum.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+public class KthLargestSubarraySum {
+
+    public static int kthLargest(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+
+                if (pq.size() < k) {
+                    pq.add(sum);
+                } else if (sum > pq.peek()) {
+                    pq.poll();
+                    pq.add(sum);
+                }
+            }
+        }
+        return pq.peek();
+    }
+
+    // Main method for VS Code testing
+    public static void main(String[] args) {
+        int[] arr1 = {20, -5, -1};
+        int k1 = 3;
+        System.out.println("The " + k1 + "rd largest subarray sum is: " + kthLargest(arr1, k1));
+
+        int[] arr2 = {10, -10, 20, -40};
+        int k2 = 6;
+        System.out.println("The " + k2 + "th largest subarray sum is: " + kthLargest(arr2, k2));
+    }
+}
